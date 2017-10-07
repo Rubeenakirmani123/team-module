1c39792864c57d5c5b2a7e42bb46c399fe7a58d1
Nazoo123 <tr.nazoo@eaoservices.com>
2017-10-07 12:53:13 +0530

    Added css folder  , images in img folder ,updated index

--- client/index.php
+++ client/index.php
@@ -4,9 +4,10 @@
     <head>
         <meta charset="UTF-8">
         <title></title>
-       <!-- <link href="./deps/bootstrap-3.3.7-dist/css/bootstrap.css" rel="stylesheet" type="text/css"/> -->
+        <link href="./deps/bootstrap-3.3.7-dist/css/bootstrap.css" rel="stylesheet" type="text/css"/> 
         <script src="deps/jquery/jquery-3.1.1.js" type="text/javascript"></script>
         <script src="js/main.js" type="text/javascript"></script>
+         <link rel="stylesheet" href="./css/main-style.css"/> 
          <style>
             .eis-team-container{
                 width: 90%;
@@ -76,8 +77,11 @@
         <div id='output'>
 
         </div>
-
-
+        <div class="content">
+            <?php
+                 require_once './partials/eis-create-team-view.php';
+              ?>
+             </div>
         <script>
 //            DisplayMembers();
         </script>
